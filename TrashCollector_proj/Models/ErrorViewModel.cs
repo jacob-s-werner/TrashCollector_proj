a97dede6297d9ebfe747c6a3324103f1a0b60911
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-17 14:23:53 -0500

    Initial commit of skeleton project.

---
+++ TrashCollector_proj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrashCollector_proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
