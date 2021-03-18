5d5967d9d64effd053f1cff5365b03ee69e29cbd
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-18 13:03:37 -0500

    Added Employee and Customer properties.

--- TrashCollector_proj/Models/Customer.cs
+++ TrashCollector_proj/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace TrashCollector_proj.Models
 {
     public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public Address Address { get; set; }
+        //public UserRole RoleId { get; set; }
+
     }
 }
